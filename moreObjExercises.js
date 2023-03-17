07c684d5f9c3089a6023424a180899f46ce205f0
Shyam <t7bhsh00@students.oamk.fi>
2023-03-17 17:41:54 +0200

    updating Obj methods

---
+++ moreObjExercises.js
@@ -0,0 +1,45 @@
+/*
+Your function must always return the entire record collection object.
+If prop isn't tracks and value isn't an empty string, update or set that album's prop to value.
+If prop is tracks but the album doesn't have a tracks property, create an empty array and add value to it.
+If prop is tracks and value isn't an empty string, add value to the end of the album's existing tracks array.
+If value is an empty string, delete the given prop property from the album.
+*/
+
+// Setup
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  // Access target album in record collection
+  const album = records[id];
+  if (value === "") {
+    delete album[prop];
+  } else if (prop !== "tracks") {
+    album[prop] = value;
+  } else {
+    album["tracks"] = album["tracks"] || [];
+    album["tracks"].push(value);
+  }
+  return records;
+}
+updateRecords(recordCollection, 2548, "artist");
+
